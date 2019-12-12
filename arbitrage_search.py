dcdc03495c17d84232dc19d67ddd543165881e46
Lane Birmingham <lanebirm2@gmail.com>
2019-12-12 21:27:48 +1000

    Further features added

--- arbitrage_search.py
+++ arbitrage_search.py
@@ -8,10 +8,13 @@ import data_sorter
 import csv
 import time_convertor
 import pickle 
+from datetime import datetime
 
 # global constants
 PULL_API_DATA = False  # pull data. if False reload data of last pull
-H2H_ODDS_THRESHOLD = 1.9 # if both teams have avalible odds above this threshold will be counted as arbitrage bet opportunity
+H2H_WIN_ODDS_THRESHOLD = 2.0 # top odds threshold
+H2H_SAFETY_ODDS_THRESHOLD = 1.75 # safety odds threshold
+REGION_CODE = 'au'
 
 def main():
 
@@ -22,16 +25,18 @@ def main():
 	if PULL_API_DATA:
 		# send api request for data for each sport
 		api_grabber = api_puller.GetData()
-		active_sports = ['americanfootball_nfl', 'aussierules_afl','basketball_euroleague','basketball_nba','cricket_test_match','icehockey_nhl','mma_mixed_martial_arts', 'rugbyleague_nrl']
+		# full list excluding soccer = ['americanfootball_nfl', 'aussierules_afl','basketball_euroleague','basketball_nba','cricket_test_match','icehockey_nhl','mma_mixed_martial_arts', 'rugbyleague_nrl']
+		active_sports = ['americanfootball_nfl','basketball_euroleague','basketball_nba','cricket_test_match','icehockey_nhl','mma_mixed_martial_arts']
 		#active_sports = ['rugbyleague_nrl']
 		
 		for sport in active_sports:
-			request_params = api_puller.SportOddsRequestParams(sport=sport, region='au', market='h2h')
+			request_params = api_puller.SportOddsRequestParams(sport=sport, region=REGION_CODE, market='h2h')
 			response_data['odds_data'].append(api_grabber.get_sport_odds(request_params))
 			
 		# save as .pickle so can be reloaded for testing. Saves api calls
 		response_data['sports'] = active_sports
 		pickle.dump(response_data, open( "save.pickle", "wb" ))
+		pickle.dump(response_data, open( "backup_pickles/save" + str(datetime.now().strftime('_%d_%Y_%m_%H_%M_%S')) + REGION_CODE + ".pickle", "wb" ))   
 	else:
 		# reload last pull data
 
@@ -42,19 +47,26 @@ def main():
 		# read from pickle file
 		response_data = pickle.load( open( "save.pickle", "rb" ) )
 
+	# sites to use
+	#sites_best = []
+	#sites_safety = []
+	sites_best = []
+	sites_safety = []
 
 	for i,odds in enumerate(response_data['odds_data']):
 		# for each sport process response
 		data_processor.append(data_sorter.DataProcessor())
 		data_processor[i].filter_data(odds)
-		data_processor[i].sort_max_h2h_odds()
-		data_processor[i].check_for_h2h_odds_at_threshold(H2H_ODDS_THRESHOLD)
+		data_processor[i].sort_max_h2h_odds(sites_best, sites_safety)
+		data_processor[i].check_for_h2h_odds_win_lose_thresholds(H2H_WIN_ODDS_THRESHOLD, H2H_SAFETY_ODDS_THRESHOLD)
 
+
+	# print results
 	for i in range(len(data_processor)):
-		if len(data_processor[i].arbitrage_bet_opportunities['odds data']) > 0:
+		if len(data_processor[i].bet_opportunities['odds data']) > 0:
 			print('Arbitrage opportunity for', data_processor[i].sports_name, ' for the following: \n ')
-			for j,event in enumerate(data_processor[i].arbitrage_bet_opportunities['odds data']):
-				print("Match Details: ", event, '  ||  Event Time: ', data_processor[i].arbitrage_bet_opportunities['start time'][j], "  ||  Time until: ", data_processor[i].start_time['time till start'][j])
+			for j,event in enumerate(data_processor[i].bet_opportunities['odds data']):
+				print("Match Details: ", event, '  ||  Event Time: ', data_processor[i].bet_opportunities['start time'][j], "  ||  Time until: ", data_processor[i].bet_opportunities['time till start'][j])
 		else:
 			print('No Arbitrage opportunities for ', data_processor[i].sports_name)
 		print('\n \n ')
--- data_sorter.py
+++ data_sorter.py
@@ -15,9 +15,10 @@ class DataProcessor():
 		self.h2h_odds = []
 		self.betting_sites = []
 		self.max_h2h_odds = []
+		self.max_safety_odds = []
 		self.sports_name = ''
 		self.start_time = {'datetime format':[] , 'string format':[], 'time till start':[]}
-		self.arbitrage_bet_opportunities = {'odds data':[] , 'match index':[], 'start time':[], 'time till start':[]}
+		self.bet_opportunities = {'odds data':[] , 'match index':[], 'start time':[], 'time till start':[]}
 
 	def filter_data(self, json_data):
 		""" process data to pull out just teams, sites and h2h odds of all events  """
@@ -57,9 +58,20 @@ class DataProcessor():
 		# print(self.betting_sites)
 		# print(self.h2h_odds)
 
-	def sort_max_h2h_odds(self):
+	def sort_max_h2h_odds(self, sites_best = [], sites_safety = []):
 		"""  sort to get max odds offered for each team for all events. """
 
+		sites_best_use_all = False
+		sites_safety_use_all = False
+
+		if (len(sites_best) == 0):
+			# no restrictions use all sites
+			sites_best_use_all = True
+		if (len(sites_safety) == 0):
+			# no restrictions use all sites
+			sites_safety_use_all = True
+
+		# calculate max_h2h_odds	
 		for i,event in enumerate(self.teams):
 			# for each event
 
@@ -69,35 +81,115 @@ class DataProcessor():
 			for j,team in enumerate(self.teams[i]):
 				# for each team
 				for k,site_odds in enumerate(self.h2h_odds[i]):
+					# restrict to sites to use. defaults to all
+					if sites_best_use_all or (self.betting_sites[i][k] in sites_best):
 					# for all odds find largest and save
-					if site_odds[j] > self.max_h2h_odds[i][team][0]:
-						self.max_h2h_odds[i][team] = [site_odds[j] , self.betting_sites[i][k]]
+						if site_odds[j] > self.max_h2h_odds[i][team][0]:
+							self.max_h2h_odds[i][team] = [site_odds[j] , self.betting_sites[i][k]]
+
+		
+		# calculate max_safety_odds	
+		for i,event in enumerate(self.teams):
+			# for each event
+			
+			# init list of 2 dictonarys (1 for each team) that contains the max odds and the site for those odds
+			self.max_safety_odds.append({self.teams[i][0] : [0, "no site"], self.teams[i][1] : [0, "no site"]})
+
+			for j,team in enumerate(self.teams[i]):
+				# for each team
+				for k,site_odds in enumerate(self.h2h_odds[i]):
+					# restrict to sites to use. defaults to all
+					if sites_safety_use_all or (self.betting_sites[i][k] in sites_safety):
+					# for all odds find largest and save
+						if site_odds[j] > self.max_safety_odds[i][team][0]:
+							self.max_safety_odds[i][team] = [site_odds[j] , self.betting_sites[i][k]]
+
 		
 		#print('Finished finding max odds')
 
-	def check_for_h2h_odds_at_threshold(self, min_h2h_alert_threshold = 2.0):
-		"""  Alert if there is an event where odds are found for both teams of an event above the threshold """
+	def check_for_h2h_odds_win_lose_thresholds(self, win_threshold = 2.0, safety_bet_threshold = 999):
+		"""  Alert if there is an event where odds are found for teams of an event above the thresholds """
 
 		if len(self.max_h2h_odds) < 0:
 			print('Have not sorted max h2h odds yet')
 			return
 		
+		if safety_bet_threshold == 999:
+			# no value given. use win threshold for both
+			safety_bet_threshold = win_threshold
+
+		for i,event in enumerate(self.teams):
+			# for each event check if max odds for both teams is above the threshold
+			team_one_above = False
+			team_two_above = False
+			team_one_stake = ""
+			team_two_stake = ""
+
+			# logic to determine what odds too print as max based on allowed sites and thresholds
+			if (self.max_h2h_odds[i][event[0]][0] > win_threshold) and  (self.max_safety_odds[i][event[1]][0] > safety_bet_threshold):
+				team_one_above = True
+				team_one_stake = "win"
+				team_two_above = True
+				team_two_stake = "safety"
+			
+			if (not team_one_above) and (not team_two_above):		
+				#dont have to worry about which is better as didnt pass thresholds in other ordering
+				if (self.max_h2h_odds[i][event[1]][0] > win_threshold) and  (self.max_safety_odds[i][event[0]][0] > safety_bet_threshold):
+					team_one_above = True
+					team_one_stake = "safety"
+					team_two_above = True
+					team_two_stake = "win"
+			elif (self.max_safety_odds[i][event[0]][0] > self.max_safety_odds[i][event[1]][0]):
+				#assume odds above thresholds found to reach above elif statement. Check if a flip ups the safety odds
+				if (self.max_h2h_odds[i][event[1]][0] > win_threshold) and  (self.max_safety_odds[i][event[0]][0] > safety_bet_threshold):
+					team_one_above = True
+					team_one_stake = "safety"
+					team_two_above = True
+					team_two_stake = "win"
+
+
+			if team_one_above and team_two_above:
+				#print('Found Arbitrage opportunity for event ', i)
+				temp_max_h2h_odds = []
+				if (team_one_stake == "win") and (team_two_stake == "safety"):
+					temp_max_h2h_odds = {event[0] : self.max_h2h_odds[i][event[0]], event[1] : self.max_safety_odds[i][event[1]]}
+				elif (team_one_stake == "safety") and (team_two_stake == "win"):
+					temp_max_h2h_odds = {event[0] : self.max_safety_odds[i][event[0]], event[1] : self.max_h2h_odds[i][event[1]]}
+
+				self.bet_opportunities['odds data'].append(temp_max_h2h_odds)
+				self.bet_opportunities['match index'].append(i)
+				self.bet_opportunities['start time'].append(self.start_time['string format'][i])
+				self.bet_opportunities['time till start'].append(self.start_time['time till start'][i])
+
+	def check_for_certain_sites(self, win_threshold = 2.0, safety_bet_threshold = 999, safety_sites = {}, win_sites = {} ):
+		"""  Alert if there is an event where odds are found for teams of an event above the thresholds """
+
+		if len(self.max_h2h_odds) < 0:
+			print('Have not sorted max h2h odds yet')
+			return
+		
+		if safety_bet_threshold == 999:
+			# no value given. use win threshold for both
+			safety_bet_threshold = win_threshold
+
 		for i,event in enumerate(self.teams):
 			# for each event check if max odds for both teams is above the threshold
 			team_one_above = False
 			team_two_above = False
 
-			if self.max_h2h_odds[i][event[0]][0] > min_h2h_alert_threshold:
+			if (self.max_h2h_odds[i][event[0]][0] > win_threshold) and  (self.max_h2h_odds[i][event[1]][0] > safety_bet_threshold):
 				team_one_above = True
-			if self.max_h2h_odds[i][event[1]][0] > min_h2h_alert_threshold:
+				team_two_above = True
+			if (self.max_h2h_odds[i][event[1]][0] > win_threshold) and  (self.max_h2h_odds[i][event[0]][0] > safety_bet_threshold):
+				team_one_above = True
 				team_two_above = True
 
 			if team_one_above and team_two_above:
 				#print('Found Arbitrage opportunity for event ', i)
-				self.arbitrage_bet_opportunities['odds data'].append(self.max_h2h_odds[i])
-				self.arbitrage_bet_opportunities['match index'].append(i)
-				self.arbitrage_bet_opportunities['start time'].append(self.start_time['string format'][i])
-				self.arbitrage_bet_opportunities['time till start'].append(self.start_time['time till start'][i])
+				self.bet_opportunities['odds data'].append(self.max_h2h_odds[i])
+				self.bet_opportunities['match index'].append(i)
+				self.bet_opportunities['start time'].append(self.start_time['string format'][i])
+				self.bet_opportunities['time till start'].append(self.start_time['time till start'][i])
 
 
 
